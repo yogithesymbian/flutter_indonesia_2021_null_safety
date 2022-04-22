14b337e1a99ceec18337054de98123a49518c68d
yogithesymbian <yogirenbox33@gmail.com>
2022-04-23 07:55:40 +0800

    Added Dialog With Get

--- lib/presentation/app_widget.dart
+++ lib/presentation/app_widget.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_indonesia_2021/presentation/splash_screen/splash_screen.dart';
+import 'package:get/get.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/presentation/user/user_page.dart
+++ lib/presentation/user/user_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class UserPage extends StatefulWidget {
   const UserPage({Key? key}) : super(key: key);
@@ -12,8 +13,113 @@ class _UserPageState extends State<UserPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Get.defaultDialog(
+            title: 'ADD USER',
+            content: AddUserDialog(),
+            // onCancel: () => Get.close(Duration.microsecondsPerSecond),
+            // onConfirm: () {
+            //   //
+            // }
+          );
+        },
         child: Text('Add'),
+      ),
+    );
+  }
+}
+
+class AddUserDialog extends StatefulWidget {
+  const AddUserDialog({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AddUserDialog> createState() => _AddUserDialogState();
+}
+
+class _AddUserDialogState extends State<AddUserDialog> {
+  TextEditingController _nmTextEditingController = TextEditingController();
+  TextEditingController _jobTextEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nmTextEditingController.dispose();
+    _jobTextEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8, right: 8),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 55.0,
+            child: TextField(
+              controller: _nmTextEditingController,
+              decoration: InputDecoration(
+                hintText: 'John',
+                labelText: 'Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                prefixIcon: Icon(Icons.person),
+                filled: true,
+                hintStyle: TextStyle(color: Colors.grey[800]),
+                fillColor: Colors.white70,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: 55.0,
+            child: TextField(
+              controller: _jobTextEditingController,
+              decoration: InputDecoration(
+                hintText: 'Software Engineer',
+                labelText: 'Job',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                prefixIcon: Icon(Icons.join_inner),
+                filled: true,
+                hintStyle: TextStyle(color: Colors.grey[800]),
+                fillColor: Colors.white70,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.cancel),
+                  label: Text('Cancel'),
+                  style: OutlinedButton.styleFrom(
+                    shape: StadiumBorder(),
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text('Add'),
+                  style: ElevatedButton.styleFrom(
+                    shape: StadiumBorder(),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
